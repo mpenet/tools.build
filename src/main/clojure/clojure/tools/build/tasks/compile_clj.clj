04f494522076e2d55e83c6678c116ed862c9d654
Max Penet <mpenet@s-exp.com>
2022-05-18 14:04:25 +0200

    add context to errors

--- src/main/clojure/clojure/tools/build/tasks/compile_clj.clj
+++ src/main/clojure/clojure/tools/build/tasks/compile_clj.clj
@@ -8,37 +8,37 @@
 
 (ns clojure.tools.build.tasks.compile-clj
   (:require
-    [clojure.java.io :as jio]
-    [clojure.pprint :as pprint]
-    [clojure.string :as str]
-    [clojure.tools.build.api :as api]
-    [clojure.tools.build.util.file :as file]
-    [clojure.tools.build.tasks.process :as process]
-    [clojure.tools.namespace.find :as find]
-    [clojure.tools.namespace.dependency :as dependency]
-    [clojure.tools.namespace.parse :as parse])
+   [clojure.java.io :as jio]
+   [clojure.pprint :as pprint]
+   [clojure.string :as str]
+   [clojure.tools.build.api :as api]
+   [clojure.tools.build.util.file :as file]
+   [clojure.tools.build.tasks.process :as process]
+   [clojure.tools.namespace.find :as find]
+   [clojure.tools.namespace.dependency :as dependency]
+   [clojure.tools.namespace.parse :as parse])
   (:import
-    [java.io File]
-    [java.nio.file Files]
-    [java.nio.file.attribute FileAttribute]))
+   [java.io File]
+   [java.nio.file Files]
+   [java.nio.file.attribute FileAttribute]))
 
 (set! *warn-on-reflection* true)
 
 (defn- write-compile-script!
   ^File [^File script-file ^File compile-dir nses compiler-opts bindings]
   (let [compile-bindings (merge bindings
-                           {#'*compile-path* (.toString compile-dir)
-                            #'*compiler-options* compiler-opts})
+                                {#'*compile-path* (.toString compile-dir)
+                                 #'*compiler-options* compiler-opts})
         binding-nses (->> compile-bindings keys
-                       (map #(.. ^clojure.lang.Var % ns name)) ;; Var->namespace
-                       distinct (remove #(= % 'clojure.core)))
+                          (map #(.. ^clojure.lang.Var % ns name)) ;; Var->namespace
+                          distinct (remove #(= % 'clojure.core)))
         requires (map (fn [n] `(require '~n)) binding-nses)
         do-compile `(with-bindings ~compile-bindings
                       ~@(map (fn [n] `(~'compile '~n)) nses)
                       (System/exit 0))
         script (->> (conj (vec requires) do-compile)
-                 (map #(with-out-str (pprint/pprint %)))
-                 (str/join (System/lineSeparator)))]
+                    (map #(with-out-str (pprint/pprint %)))
+                    (str/join (System/lineSeparator)))]
     (spit script-file script)))
 
 (defn- ns->path
@@ -54,27 +54,27 @@
   (let [ns-decls (mapcat find/find-ns-decls-in-dir dirs)
         ns-candidates (set (map parse/name-from-ns-decl ns-decls))
         graph (reduce
-                (fn [graph decl]
-                  (let [sym (parse/name-from-ns-decl decl)]
-                    (reduce
-                      (fn [graph dep] (dependency/depend graph sym dep))
-                      graph
-                      (parse/deps-from-ns-decl decl))))
-                (dependency/graph)
-                ns-decls)]
+               (fn [graph decl]
+                 (let [sym (parse/name-from-ns-decl decl)]
+                   (reduce
+                    (fn [graph dep] (dependency/depend graph sym dep))
+                    graph
+                    (parse/deps-from-ns-decl decl))))
+               (dependency/graph)
+               ns-decls)]
     (->> graph
-      dependency/topo-sort
-      (filter ns-candidates) ;; only keep stuff in these dirs
-      (concat ns-candidates) ;; but make sure everything is in there at least once
-      distinct)))
+         dependency/topo-sort
+         (filter ns-candidates) ;; only keep stuff in these dirs
+         (concat ns-candidates) ;; but make sure everything is in there at least once
+         distinct)))
 
 (defn- basis-paths
   "Extract all path entries from basis, in classpath order"
   [{:keys [classpath classpath-roots]}]
   (let [path-set (->> classpath
-                   (filter #(contains? (val %) :path-key))
-                   (map key)
-                   set)]
+                      (filter #(contains? (val %) :path-key))
+                      (map key)
+                      set)]
     (filter path-set classpath-roots)))
 
 (defn compile-clj
@@ -95,13 +95,14 @@
         ;; java-command will run in context of *project-dir* - basis, classpaths, etc
         ;; should all be relative to that (or absolute like working-compile-dir)
         process-args (process/java-command (merge
-                                             (select-keys params [:java-cmd :java-opts :use-cp-file])
-                                             {:cp [(.getPath working-compile-dir) class-dir]
-                                              :basis basis
-                                              :main 'clojure.main
-                                              :main-args [(.getCanonicalPath compile-script)]}))
+                                            (select-keys params [:java-cmd :java-opts :use-cp-file])
+                                            {:cp [(.getPath working-compile-dir) class-dir]
+                                             :basis basis
+                                             :main 'clojure.main
+                                             :main-args [(.getCanonicalPath compile-script)]}))
         _ (spit (jio/file working-dir "compile.args") (str/join " " (:command-args process-args)))
-        exit (:exit (process/process process-args))]
+        proc (process/process process-args)
+        exit (:exit proc)]
     (if (zero? exit)
       (do
         (if (seq filter-nses)
@@ -109,4 +110,6 @@
           (file/copy-contents working-compile-dir compile-dir-file))
         ;; only delete on success, otherwise leave the evidence!
         (file/delete working-dir))
-      (throw (ex-info (str "Clojure compilation failed, working dir preserved: " (.toString working-dir)) {})))))
+      (throw (ex-info (str "Clojure compilation failed, working dir preserved: "
+                           (.toString working-dir))
+                      proc)))))
